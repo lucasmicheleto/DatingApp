138f855dcc4b9e70df6d3f86712926a5c880a623
lucasnovo <lucasnovo@outlook.com>
2023-08-09 02:00:46 -0300

    cache members on service

--- ClientApp/src/app/members/member-list/member-list.component.ts
+++ ClientApp/src/app/members/member-list/member-list.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Member } from '../member';
 import { MembersService } from 'src/app/services/members.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
-export class MemberListComponent {
-  public members: Member[] = [];
+export class MemberListComponent implements OnInit {
+  public members$!: Observable<Member[]>;
 
   constructor(private membersService: MembersService) {
-    this.membersService.getMembers().subscribe({
-      next: res => this.members = res
-    })
+  }
+  ngOnInit(): void {
+    this.members$ = this.membersService.getMembers();
   }
 }
--- ClientApp/src/app/services/members.service.ts
+++ ClientApp/src/app/services/members.service.ts
@@ -2,26 +2,37 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Member } from '../members/member';
 import { User } from '../models/user';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
-
+  members: Member[] = [];
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'api/users');
+    return this.members.length > 0 ? 
+    of(this.members) : 
+    this.http.get<Member[]>(this.baseUrl + 'api/users')
+    .pipe(map(members => {
+      this.members = members;
+      return members;
+    }));
   }
 
   getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + 'api/users/'+username);
+    const member = this.members.find(x => x.userName === username);
+    return member ? of(member) : this.http.get<Member>(this.baseUrl + 'api/users/'+username);
   }
 
   updateMember(member: Member){
-    return this.http.put(this.baseUrl + "api/users", member);
+    return this.http.put(this.baseUrl + "api/users", member)
+      .pipe(map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = { ...this.members[index], ...member };
+      }));
   }
-
 
   getHttpOptions(){
     var rawUser = localStorage.getItem("user");
